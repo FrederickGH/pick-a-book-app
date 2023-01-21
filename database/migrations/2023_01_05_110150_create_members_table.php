3f68933cd08c9532c4eff2a8a178afad894cc750
FrederickGH <121942117+FrederickGH@users.noreply.github.com>
2023-01-22 01:42:46 +0700

    Update 2023_01_05_110150_create_members_table.php
    Pemasukan isi tabel untuk member

--- database/migrations/2023_01_05_110150_create_members_table.php
+++ database/migrations/2023_01_05_110150_create_members_table.php
@@ -15,6 +15,17 @@ class CreateMembersTable extends Migration
     {
         Schema::create('members', function (Blueprint $table) {
             $table->id();
+            $table->string('NIK')->unique();
+            $table->string('id_card');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('password')
+            $table->string('address');
+            $table->date('DOB');
+            $table->string('phonenum');
+            $table->integer('borrowCount')->default('0');
+            $table->decimal('Balance', 10, 2)->default('0.0');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
